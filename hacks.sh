c233d680fa1af9dc424284bc52cb41b0daa077f1
Adam Purdie <spidey@amazon.com>
2022-05-10 16:42:24 +1000

    reverting reverted reversion

--- hacks.sh
+++ hacks.sh
@@ -27,21 +27,48 @@ stopwatch() {
         done
 }
 
-### Ros1 hacks
-switch-project() {
+### some scrawled together script to see if a file is not referenced in another directory
+#    useful for listing unused images in a doc tree
+
+find_usage () {
+  SOURCE_FOLDER=$1
+  CONTENT_FOLDER=$2
+
+  echo $SOURCE_FOLDER $CONTENT_FOLDER
+
+  return 0
+
+  FILE_LIST=`ls ${SOURCE_FOLDER}`
+
+  for FILE_ITEM in ${FILE_LIST};
+  do 
+    # echo "processing $f"; 
+    MATCH=`grep -r ${FILE_ITEM} ${CONTENT_FOLDER} | wc -l`
+    if [ ${MATCH} -lt 1 ]; 
+      then echo "${FILE_ITEM} not used"; 
+    fi
+  done
+}
+
+## Ros project hacks - takes an input as a folder path, sources ROS and project code
+## Takes an input, stores it in the .tmp.last_ros_project file
+## if there's no input it gets the last-used folder
+ros-project() {
     if [ -z ${1} ] ; then # no project
         PROJECTDIR=`cat ${PROFILE_DIR}/.tmp.last_ros_project`
-        echo "No ROS project specified going with last: ${PROJECTDIR}";
+        echo "Using previous project dir: ${PROJECTDIR}";
     else 
         PROJECTDIR=$1
+        echo "Selected ros project at: ${PROJECTDIR}"
     fi
+
 
     echo ${PROJECTDIR} > ${PROFILE_DIR}/.tmp.last_ros_project
     export WORKSPACE=${PROJECTDIR}
     source /opt/ros/${ROSDIST}/setup.bash
 
     # ROSDIST custom defines (not implemented)
-    ROS1LIST="bionic melodic"
+    ROS1LIST="bionic melodic noetic"
     ROS2LIST="dashing eloquent foxy galactic"
 
     if [[ " $ROS1LIST " =~ .*\ $ROSDIST\ .* ]]; then
@@ -51,10 +78,15 @@ switch-project() {
     if [[ " $ROS2LIST " =~ .*\ $ROSDIST\ .* ]]; then
 	source ${WORKSPACE}/install/local_setup.bash
         echo "ROS 2 - ${ROSDIST}"
+        source /usr/share/colcon_cd/function/colcon_cd.sh
         return;
     fi
 
     echo "${ROS1LIST} RosDist not found"
 }
 
+## Same as above but uses the pwd as the folder
+ros-project-pwd() {
+    ros-project `pwd`
+}
 
