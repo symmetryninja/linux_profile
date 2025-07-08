3a747e9beafc53b4ef793cd696c0d7bfdd9de726
spidey <adam@symmetry.ninja>
2025-07-08 22:10:09 +1000

    fixing up oversealous path additions

--- profile.sh
+++ profile.sh
@@ -10,7 +10,7 @@ source ${LINUX_PROFILE_DIR}/git-completion.bash
 
 ## pyenv configs
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+[[ ! "${PATH}" == *"${PYENV_ROOT}"* ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
